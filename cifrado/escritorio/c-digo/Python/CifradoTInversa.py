92f9c032ae171b42ac4e2aca51208a522046897a
VillanuevaCruzJesusAntonio <45885348+VillanuevaCruzJesusAntonio@users.noreply.github.com>
2018-12-15 19:21:56 -0600

    Create CifradoTInversa.py

---
+++ cifrado/escritorio/c-digo/Python/CifradoTInversa.py
@@ -0,0 +1,23 @@
+class cifradoTInversa:
+
+    def __init__(self , textoEntrada):
+        self.txt = textoEntrada
+        self.textoSalida = ""
+        self.vacio = ""
+
+    def cifra(self,txtnew):
+        self.vacio = self.textoSalida
+        self.textoSalida = txtnew[::-1]
+        return self.textoSalida
+
+    def front(self ,letra):
+        self.vacio = self.textoSalida
+        self.textoSalida = letra +self.textoSalida
+
+    def back(self):
+        aux = self.vacio
+        self.vacio = self.textoSalida
+        self.textoSalida = aux
+
+    def __str__(self):
+        return self.textoSalida
