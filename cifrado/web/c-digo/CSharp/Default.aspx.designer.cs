6c841109d28374e8e0ab04d39784a2d0d0bbb951
VillanuevaCruzJesusAntonio <45885348+VillanuevaCruzJesusAntonio@users.noreply.github.com>
2018-12-15 22:36:47 -0600

    Create Default.aspx.designer.cs

---
+++ cifrado/web/c-digo/CSharp/Default.aspx.designer.cs
@@ -0,0 +1,20 @@
+namespace Cifrado {
+    
+    
+    public partial class Default {
+        
+        protected System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        protected System.Web.UI.WebControls.TextBox textoPlano;
+        
+        protected System.Web.UI.WebControls.TextBox txtDesplazamiento;
+        
+        protected System.Web.UI.WebControls.Button btnConvertir;
+        
+        protected System.Web.UI.HtmlControls.HtmlGenericControl txtCifradoCesar;
+        
+        protected System.Web.UI.HtmlControls.HtmlGenericControl txtCifradoInverso;
+        
+        protected System.Web.UI.HtmlControls.HtmlGenericControl txtCifradoInversoGrupal;
+    }
+}
