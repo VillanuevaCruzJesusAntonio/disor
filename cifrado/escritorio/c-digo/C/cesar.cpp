00296e7e6a8b9835ad76ed8dc909f5ff53e74ca2
VillanuevaCruzJesusAntonio <45885348+VillanuevaCruzJesusAntonio@users.noreply.github.com>
2018-12-15 18:37:19 -0600

    Add files via upload

---
+++ cifrado/escritorio/c-digo/C/cesar.cpp
@@ -0,0 +1,66 @@
+#include "cesar.hpp"
+
+Cesar::Cesar(string textEntrada,int posicion)
+{
+    this->texto=textEntrada;
+    this->indicePos=posicion;
+}
+
+string Cesar::cifrar(){
+    string textoCifrado;
+    int indice;
+    int movimiento;
+    size_t dimension;
+    for(indice=0;indice<texto.length();indice++){
+        if(texto.at(indice)>96 && texto.at(indice)<123)
+            texto.at(indice) -= 32;
+
+        if(texto.at(indice)<64 && texto.at(indice)>91)
+            textoCifrado+=texto.at(indice);
+
+        else{
+            dimension = letras.find(texto.at(indice));
+            if(((int)dimension+indicePos) > letras.length())
+            movimiento = ((int)dimension+indicePos)-letras.length();
+            else
+            movimiento = (int)dimension+indicePos;
+            textoCifrado.append(letras,movimiento,1);
+        }
+    }
+    return textoCifrado;
+}
+
+string Cesar::decifrar(){
+    string textoDesifrado;
+    int indice;
+    int movimiento;
+    size_t dimension;
+    for(indice=0;indice<texto.length();indice++){
+        if(texto.at(indice)>96 && texto.at(indice)<123)
+            texto.at(indice) -= 32;
+        //Paso las letras minusculas a mayusculas
+
+        if(texto.at(indice)<64 && texto.at(indice)>91)
+            textoDesifrado+=texto.at(indice);
+        //Si el caracter actual no es una letra se
+        //deja sin cifrar 
+
+        else{
+            dimension = letras.find(texto.at(indice));
+            if(((int)dimension-indicePos) < 0)
+                movimiento = letras.length()+((int)dimension-indicePos);
+            else
+                movimiento = (int)dimension-indicePos;
+            //Revision para evitar que el valor quede
+            //fuera de rango, como la resta general un
+            //valor negativo lo sumo al tamaño del
+            //alfabeto para que avanze hacia atras
+
+            textoDesifrado.append(letras,movimiento,1);
+        }
+    }
+    return textoDesifrado;
+}
+
+
+
